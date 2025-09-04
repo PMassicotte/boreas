778770b6c3d9d2736ba65b557ad187654c39957d
Philippe Massicotte <pmassicotte@hotmail.com>
2025-09-04 14:13:17 -0400

    refactor(lut): streamline LUT initialization and add bounds checking for indices

--- src/lut/mod.rs
+++ src/lut/mod.rs
@@ -1,5 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+// [wavelength][theta][ozone][taucl][albedo]
+type LutArray = Box<[[[[[f32; 7]; 8]; 10]; 19]; 83]>;
+
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct Lut {
     xthetas: Vec<f32>,
@@ -7,7 +12,7 @@ pub struct Lut {
     xtaucl: Vec<f32>,
     xalb: Vec<f32>,
     wavelengths: Vec<f32>,
-    ed_lut: [[[[[f32; 7]; 8]; 10]; 19]; 83], // [wavelength][theta][ozone][taucl][albedo]
+    ed_lut: LutArray,
 }
 
 impl Lut {
@@ -17,41 +22,17 @@ impl Lut {
     /// 3. Ozone = 100 : 550 : 50
     /// 4. Cloud optical Thickness = 0 to 64 = c(0,1,2,4,8,16,32,64)
     /// 5. Surface Albedo = 0.05 : 0.9 : 0.15
-    pub fn new() -> Self {
-        // Thetas: 0 to 90, by 5
-        let xthetas: Vec<f32> = (0..=90).map(|i| (i * 5) as f32).collect();
-
-        // Ozone: 100 to 550, 50 steps (10 values)
+    pub fn from_file(filename: &str) -> Result<Self, std::io::Error> {
+        let xthetas: Vec<f32> = (0..19).map(|i| (i * 5) as f32).collect();
         let xozone: Vec<f32> = (0..10).map(|i| 100.0 + (i * 50) as f32).collect();
-
-        // Cloud optical depth (8 values)
         let xtaucl: Vec<f32> = vec![0.0, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0];
-
-        // Surface albedo: 0.05 to 0.95, 0.15 steps (7 values)
         let xalb: Vec<f32> = vec![0.05, 0.2, 0.35, 0.5, 0.65, 0.8, 0.95];
-
-        // Wavelengths: 290 to 700 nm, 5 nm steps (83 values)
         let wavelengths: Vec<f32> = (0..83).map(|i| 290.0 + (i * 5) as f32).collect();
 
-        Lut {
-            xthetas,
-            xozone,
-            xtaucl,
-            xalb,
-            wavelengths,
-            ed_lut: [[[[[0.0; 7]; 8]; 10]; 19]; 83],
-        }
-    }
-
-    pub fn load_from_file(&mut self, filename: &str) -> Result<(), std::io::Error> {
         let file = File::open(filename)?;
         let reader = BufReader::new(file);
         let mut values: Vec<f32> = Vec::with_capacity(
-            self.xthetas.len()
-                * self.xozone.len()
-                * self.xtaucl.len()
-                * self.xalb.len()
-                * self.wavelengths.len(),
+            xthetas.len() * xozone.len() * xtaucl.len() * xalb.len() * wavelengths.len(),
         );
 
         // Read all values from file
@@ -65,14 +46,17 @@ impl Lut {
         }
 
         // Fill the lookup table following C++ order: theta, ozone, taucl, albedo, wavelength
+        let mut ed_lut = [[[[[0.0; 7]; 8]; 10]; 19]; 83];
         let mut idx = 0;
-        for theta in 0..self.xthetas.len() {
-            for ozone in 0..self.xozone.len() {
-                for taucl in 0..self.xtaucl.len() {
-                    for albedo in 0..self.xalb.len() {
-                        for wavelength in 0..self.wavelengths.len() {
+
+        #[allow(clippy::needless_range_loop)]
+        for theta in 0..xthetas.len() {
+            for ozone in 0..xozone.len() {
+                for taucl in 0..xtaucl.len() {
+                    for albedo in 0..xalb.len() {
+                        for wavelength in 0..wavelengths.len() {
                             if idx < values.len() {
-                                self.ed_lut[wavelength][theta][ozone][taucl][albedo] = values[idx];
+                                ed_lut[wavelength][theta][ozone][taucl][albedo] = values[idx];
                                 idx += 1;
                             }
                         }
@@ -81,7 +65,14 @@ impl Lut {
             }
         }
 
-        Ok(())
+        Ok(Lut {
+            xthetas,
+            xozone,
+            xtaucl,
+            xalb,
+            wavelengths,
+            ed_lut: Box::new(ed_lut),
+        })
     }
 
     pub fn get_wavelength_values(
@@ -90,11 +81,40 @@ impl Lut {
         ozone_idx: usize,
         taucl_idx: usize,
         albedo_idx: usize,
-    ) -> Vec<f32> {
-        (0..self.wavelengths.len())
+    ) -> Result<Vec<f32>, String> {
+        if theta_idx >= self.xthetas.len() {
+            return Err(format!(
+                "theta_idx {} out of bounds (max: {})",
+                theta_idx,
+                self.xthetas.len() - 1
+            ));
+        }
+        if ozone_idx >= self.xozone.len() {
+            return Err(format!(
+                "ozone_idx {} out of bounds (max: {})",
+                ozone_idx,
+                self.xozone.len() - 1
+            ));
+        }
+        if taucl_idx >= self.xtaucl.len() {
+            return Err(format!(
+                "taucl_idx {} out of bounds (max: {})",
+                taucl_idx,
+                self.xtaucl.len() - 1
+            ));
+        }
+        if albedo_idx >= self.xalb.len() {
+            return Err(format!(
+                "albedo_idx {} out of bounds (max: {})",
+                albedo_idx,
+                self.xalb.len() - 1
+            ));
+        }
+
+        Ok((0..self.wavelengths.len())
             .map(|wavelength_idx| {
                 self.ed_lut[wavelength_idx][theta_idx][ozone_idx][taucl_idx][albedo_idx]
             })
-            .collect()
+            .collect())
     }
 }
