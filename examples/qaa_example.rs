30b74dabd477a5e4b6f6990553552cc7cf10a51b
Philippe Massicotte <pmassicotte@hotmail.com>
2025-09-08 14:37:49 -0400

    feat(examples): add qaa_example.rs to demonstrate QAA algorithm usage with simulated SeaWiFS data

---
+++ examples/qaa_example.rs
@@ -0,0 +1,20 @@
+use boreas::iop::qaa_v6;
+use boreas::sat_bands::Satellites;
+use std::collections::BTreeMap;
+
+fn main() {
+    // Simulate rrs values are SeaWiFS wavelengths
+    let rrs = BTreeMap::from([
+        (410, 0.001974),
+        (443, 0.002570),
+        (490, 0.002974),
+        (555, 0.001670),
+        (670, 0.000324),
+    ]);
+
+    let result = qaa_v6(&rrs, Satellites::Modis);
+    println!("Chlorophyll-a: {:.3} mg/m3", result.chla);
+
+    println!("{:#?}", result);
+    println!("{:#?}", result.get_messages());
+}
